6e091157b427f165e9cda86c98e004970d37efe7
Carole ROLLAND GRELLETY <carole.grellety@gmail.com>
2025-07-26 17:55:37 +0200

    Développement Home

---
+++ frontend/src/data/featuresData.js
@@ -0,0 +1,28 @@
+import ChatIcon from '../assets/img/icon-chat.png'
+import MoneyIcon from '../assets/img/icon-money.png'
+import SecuIcon from "../assets/img/icon-security.png"
+
+
+export default [
+    {
+        "id": "1",
+        "icon": ChatIcon,
+        "alt": "Chat Icon",
+        "title": "You are our #1 priority",
+        "description": "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
+    },
+    {
+        "id": "2",
+        "icon": MoneyIcon,
+        "alt": "Money Icon",
+        "title": "More savings means higher rates",
+        "description": "The more you save with us, the higher your interest rate will be!"
+    },
+    {
+        "id": "3",
+        "icon": SecuIcon,
+        "alt": "Security Icon",
+        "title": "Security you can trust",
+        "description": "We use top of the line encryption to make sure your data and money is always safe. "
+    }
+]
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,32 @@
+import Feature from "../components/Feature"
+import featuresData from '../data/featuresData'
 
 export default function Home () {
-    
+    return (
+        <>
+            <div className='banner'>
+                <article>
+                    <h2 className="sr-only">Promoted Content</h2>
+                    <p className="subtitle">No fees.</p>
+                    <p className="subtitle">No minimum deposit.</p>
+                    <p className="subtitle">High interest rates.</p>
+                    <p className="text">Open a savings account with Argent Bank today!</p>
+                </article>
+            </div>
+            <section className="features">
+                <h2 className="sr-only">Features</h2>
+                {
+                    featuresData.map(feature => 
+                        <Feature 
+                            id={feature.id}
+                            icon={feature.icon}
+                            alt={feature.alt}
+                            title={feature.title}
+                            description={feature.description}
+                        />
+                    )
+                }
+            </section>
+        </>
+    )
 }
