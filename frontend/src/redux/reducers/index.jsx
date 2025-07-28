d35d0b61d55d17a9b9ea2a7a9d616837f190d1e9
Carole ROLLAND GRELLETY <carole.grellety@gmail.com>
2025-07-28 16:24:22 +0200

    Mise en place state manager Redux

---
+++ frontend/src/redux/reducers/index.jsx
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import userReducer from "./user.reducer";
+import loginReducer from "./login.reducer";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  login: loginReducer
+});
+
+export default rootReducer;
