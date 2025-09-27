77c8e5b999689538c05b80d7ef89a15ca58d40d3
Carole ROLLAND GRELLETY <carole.grellety@gmail.com>
2025-09-27 20:30:06 +0200

    Préparation deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ROLLAND_GRELLETY_Carole_13_argentbank_072025/', 
   plugins: [react()],
 })
